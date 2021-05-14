d83fe8d30c9840cc33007a117c8fe4e78a316287
Sabar Ahuja <33763938+sabar183ahuja@users.noreply.github.com>
2021-05-14 11:43:16 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/sabar/SoccerScoresApp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sabar/SoccerScoresApp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fionicholas.footballapp", appContext.packageName)
+        assertEquals("com.sabars.SoccerScoresApp", appContext.packageName)
     }
 }
